4bc085fb87d38ccf9ad04e0bb5ecf495c49a4fe1
sn0w <audrey.wai@sjsu.edu>
2026-09-23 22:47:36 -0700

    tut 8

---
+++ tutorials/2-css/04-css-portfolio-grid/js/hamburger.js
@@ -0,0 +1,8 @@
+// console.log("meow");
+
+const hamburger = document.querySelector(".hamburger");
+const menu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", () => {
+    menu.classList.toggle("active");
+})
